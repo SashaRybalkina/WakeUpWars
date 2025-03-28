a4f6ae621f49d20cf7cdf766f62a0a12d35ce309
SashaRybalkina <sasharybalkina2003@gmail.com>
2025-03-27 19:36:01 -0600

    Make start screen be Sudoku temporarily

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,17 +6,22 @@ import 'expo-router/entry';
 import { NavigationContainer } from '@react-navigation/native';
 
 import LoginScreen from './pages/Login';
+import InputOutput from './pages/mainPage';
 import SignUpScreen from './pages/SignUp';
 import StartScreen from './pages/StartScreen';
-import InputOutput from './pages/mainPage';
-
+import SudokuScreen from './pages/SudokuScreen';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
+      <Stack.Navigator initialRouteName="Sudoku">
+        <Stack.Screen
+          name="Sudoku"
+          component={SudokuScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Start"
           component={StartScreen}
