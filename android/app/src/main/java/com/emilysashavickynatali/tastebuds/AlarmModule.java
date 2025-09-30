fca0e1d5529ee06df5ddb0ff41b4a5a094ed019a
25hlarsen4 <u1341754@umail.utah.edu>
2025-09-29 22:36:41 -0600

    navigation issues

--- android/app/src/main/java/com/emilysashavickynatali/tastebuds/AlarmModule.java
+++ android/app/src/main/java/com/emilysashavickynatali/tastebuds/AlarmModule.java
@@ -95,20 +95,20 @@ public class AlarmModule extends ReactContextBaseJavaModule {
         }
     }
 
-    @ReactMethod
-    public void clearLaunchIntent(Promise promise) {
-        try {
-            if (getCurrentActivity() != null) {
-                Intent intent = getCurrentActivity().getIntent();
-                if (intent != null) {
-                    intent.removeExtra("screen");
-                    intent.removeExtra("params");
-                }
-            }
-            promise.resolve(true);
-        } catch (Exception e) {
-            promise.reject("CLEAR_INTENT_ERROR", e);
-        }
-    }
+    // @ReactMethod
+    // public void clearLaunchIntent(Promise promise) {
+    //     try {
+    //         if (getCurrentActivity() != null) {
+    //             Intent intent = getCurrentActivity().getIntent();
+    //             if (intent != null) {
+    //                 intent.removeExtra("screen");
+    //                 intent.removeExtra("params");
+    //             }
+    //         }
+    //         promise.resolve(true);
+    //     } catch (Exception e) {
+    //         promise.reject("CLEAR_INTENT_ERROR", e);
+    //     }
+    // }
 
 }
