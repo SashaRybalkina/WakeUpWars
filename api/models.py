f5e05f3d235691fa74da884d106f1a0ab6a66c15
SashaRybalkina <sasharybalkina2003@gmail.com>
2025-11-14 11:42:41 -0700

    fixed bug

--- api/models.py
+++ api/models.py
@@ -145,7 +145,7 @@ class UserNotification(models.Model):
     def members(self):
         if not self.challengeId:
             return User.objects.none()
-        return User.objects.filter(challengememberships__challengeID=self.challengeId)
+        return User.objects.filter(challengemembership__challengeID=self.challengeId)
 
     def __str__(self):
         return f"Notification for {self.user.username if self.user else 'Unknown'}: {self.title}"
